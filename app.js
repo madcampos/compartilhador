8ce7b0dd1bbbfecc70ff5636262a3dff25362b4c
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2015-01-31 15:21:21 -0300

    Limpeza de arquivos

--- app.js
+++
@@ -1,1 +0,0 @@
-var pubsub = require('./src/rules/rules.js');
--- src/rules/rules.js
+++
@@ -1,5 +0,0 @@
-var client = require('./client.js');
-var server = require('./server.js');
-var frontend = require('./frontend.js');
-
-//TODO: organizar as regras de negócio e definir interfaces
