9b566ef5bf22f2c7fb9455628dd315626a6a4e18
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2015-02-19 12:54:40 -0300

    Adicionado app.js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 'use strict';
 const PORT = 1337;
 const MAX_CLIENTS = 1024;
-const SERVER_UPDATE_TIMER = 1000 * 60 * 15 //15min
-const SERVER_STABILIZATION_TIMER = 1000 * 60 * 3 //3min
+const SERVER_UPDATE_TIMER = 1000 * 60 * 15; //15min
+const SERVER_STABILIZATION_TIMER = 1000 * 60 * 3; //3min
 
 let colors = require('colors');
+let entity;
 
 colors.setTheme({
 	silly: 'rainbow',
